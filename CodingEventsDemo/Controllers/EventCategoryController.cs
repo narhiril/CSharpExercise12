c25c43a7a00f7f9b2bfde612c5def0d042c57d12
narhiril <roxannarusbarsky@yahoo.com>
2022-11-09 20:05:56 -0600

    studio yay!

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,31 @@ namespace CodingEventsDemo.Controllers
         public EventCategoryController(EventDbContext dbContext)
         {
             context = dbContext;
+        }
+
+        [Route("EventCategory/Create")]
+        public IActionResult Create()
+        {
+            return View(new AddEventCategoryViewModel());
+        }
+
+        [HttpPost]
+        [Route("EventCategory/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory 
+                { 
+                    Name = model.Name,
+                };
+
+                context.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("Index");
+            }
+
+            return View();
         }
 
         public IActionResult Index()
